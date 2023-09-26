1aa2d182479f1a3ebc6fb2cfa45fad0ec5482196
Reuven Lerner <reuven@lerner.co.il>
2023-09-26 21:24:48 +0300

    gitautosync update 76

--- menu.py
+++ menu.py
@@ -6,3 +6,8 @@ def menu(*args):   # any number of positional arguments; args is then a tuple co
             return s     # did the user choose one of the elements of args? Return it!
 
         print(f'{s} is not a valid option; try again!')
+
+# were we run interactively, and *NOT* imported?
+# give the user a demo of our menu function!
+if __name__ == '__main__':
+    user_choice = menu
