90dda0b002616566606a04892ac35ceb0b9d00fe
Reuven Lerner <reuven@lerner.co.il>
2023-09-26 20:15:56 +0300

    gitautosync update 9

--- ipython_log.py
+++ ipython_log.py
@@ -6,3 +6,8 @@
 import random              # imported the "random" module
 random.randint(0, 100)     # invoked the randint function defined in the random module, passing it 0 and 100
 type(random)
+# you cannot have a module "random" and also a function "random" and also a variable "random"
+# there can be only one value assigned to the "random" variable at any given time!
+# the latest one that was defined works and sticks around... the others are gone
+
+type(random)
