2f987ebfa9ba3a601d60cd3624c03f3f41aeb943
Reuven Lerner <reuven@lerner.co.il>
2023-09-26 20:12:09 +0300

    gitautosync update 6

--- ipython_log.py
+++ ipython_log.py
@@ -1,3 +1,7 @@
 # IPython log file
 
 
+# simple example
+
+import random              # imported the "random" module
+random.randint(0, 100)     # invoked the randint function defined in the random module, passing it 0 and 100
