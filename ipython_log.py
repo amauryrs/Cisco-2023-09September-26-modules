e1feb437fdb736422877e6e60c5f02d61c199007
Reuven Lerner <reuven@lerner.co.il>
2023-09-26 20:17:01 +0300

    gitautosync update 11

--- ipython_log.py
+++ ipython_log.py
@@ -11,3 +11,10 @@ type(random)
 # the latest one that was defined works and sticks around... the others are gone
 
 type(random)
+# what names are defined? We imported the module to get functions, objects, and data
+# what did we get?
+
+# we can use the "dir" function on our module to get a list of its attributes, names that come after a .
+# dir won't tell us the type of value on each attribute, but it will tell us what was defined.
+
+dir(random)
